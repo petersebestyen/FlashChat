0518f32e72b72cc74f0036de081bc6e2ae7d3747
Peter Sebestyen <peter.sebestyen@mac.com>
2024-03-22 16:16:19 +0100

    Latest version?

---
+++ Flash Chat iOS13/App/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Péter Sebestyén on 2024.03.21.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Péter Sebestyén on 2024.03.21.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
